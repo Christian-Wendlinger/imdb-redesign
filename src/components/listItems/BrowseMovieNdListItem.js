a9bf60760c0aa02e3f630ba223ca83ced9babe48
Luca Uihlein <luca.uihlein@uni-ulm.de>
2021-03-25 00:31:35 +0100

    weird final version not detailed

--- src/components/listItems/BrowseMovieNdListItem.js
+++ src/components/listItems/BrowseMovieNdListItem.js
@@ -1,7 +1,7 @@
 import {Grid, makeStyles, Typography} from "@material-ui/core";
 import {AddBox, Star, StarBorder} from "@material-ui/icons";
 
-const ruleMargin = 5;
+const ruleMargin = 15;
 
 const useStyle = makeStyles({
     balken: {
@@ -28,55 +28,65 @@ export default function ActorListItem({
     const classes = useStyle();
     return (
         [
-            <Grid item>
-                <Grid container spacing={2} alignItems={"center"}>
-                    <Grid item xs={1}>
-                        <img src={image} width={"100%"}/>
-                    </Grid>
-
-
-                    <Grid item xs={7}>
-                        <Grid container direction={"column"} spacing={4}>
-                            <Grid item>
-                                    <Typography variant={"h2"}
-                                                color={"primary"}>{number !== undefined && number + "."} {title} </Typography>
+            <Grid container direction={"column"} spacing={6}>
+                <Grid item>
+                        <Grid container spacing={0} alignItems={"center"}>
+                            <Grid container direction={"column"} spacing={0} xs={2}>
+                                <Grid item>
+                                    <img src={image} width={"70%"}/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant={"body1"} classtitle={classes.subtitle}>
+                                             &nbsp;
+                                    </Typography>
+                                </Grid>
                             </Grid>
 
 
-                            <Grid item xs={9}>
-                                <Grid container spacing={0}>
-                                    <Grid item>
-                                                <Star color={"primary"} className={classes.starIcon}/>
-                                    </Grid>
-                                    <Grid item xs={9}>
-                                        <Typography variant={"body1"} classtitle={classes.subtitle}>
-                                                {rating} votes: {votes !== undefined && votes} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; gross: {gross !== undefined && gross}
-                                        </Typography>
+                        <Grid item xs={7}>
+                            <Grid container direction={"column"} spacing={5}>
+                                <Grid item>
+                                        <Typography variant={"h2"}
+                                                    color={"primary"}>{number !== undefined && number + "."} {title} </Typography>
+                                </Grid>
+
+
+                                <Grid item xs={9}>
+                                    <Grid container spacing={0}>
+                                        <Grid item>
+                                                    <Star color={"primary"} className={classes.starIcon}/>
+                                        </Grid>
+                                        <Grid item xs={9}>
+                                            <Typography variant={"body1"} classtitle={classes.subtitle}>
+                                                    {rating} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; votes: {votes !== undefined && votes} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; gross: {gross !== undefined && gross}
+                                            </Typography>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Grid container spacing={0}>
-                            <Grid item>
-                                        <StarBorder color={"primary"} className={classes.starIcon}/>
-                            </Grid>
-                            <Grid item xs={9}>
-                                <Typography variant={"body1"} classtitle={classes.subtitle}>
-                                    &nbsp;Rate
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                        <AddBox color={"primary"} className={classes.starIcon}/>
+                        <Grid item xs={3}>
+                            <Grid container spacing={0}>
+                                <Grid item>
+                                            <StarBorder color={"primary"} className={classes.starIcon}/>
+                                </Grid>
+                                <Grid item xs={9}>
+                                    <Typography variant={"body1"} classtitle={classes.subtitle}>
+                                        &nbsp;Rate
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                            <AddBox color={"primary"} className={classes.starIcon}/>
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>,
-            <Grid item style={{width: "100%", display: lastItem ? "none" : "block"}}>
-                <hr classtitle={classes.balken}/>
+                <Grid item style={{width: "100%", display: lastItem ? "none" : "block"}}>
+                <hr className={classes.balken}/>
             </Grid>
+            </Grid>,
+
         ]
     );
 }
